4c86b0d262f435d9ca830186992473c416621a5a
48536 <mateusz.karpeta@interia.pl>
2020-01-29 16:30:08 +0100

    Mapper update

--- src/main/java/pl/bikepoint/bikeRentalApp/mapper/Mapper.java
+++ src/main/java/pl/bikepoint/bikeRentalApp/mapper/Mapper.java
@@ -1,13 +1,24 @@
 package pl.bikepoint.bikeRentalApp.mapper;
 
 import pl.bikepoint.bikeRentalApp.dao.bike.BikeDao;
+import pl.bikepoint.bikeRentalApp.dao.bike.BikeTypeDao;
+import pl.bikepoint.bikeRentalApp.dao.bike.BrandDao;
+import pl.bikepoint.bikeRentalApp.dao.contract.OrderDao;
+import pl.bikepoint.bikeRentalApp.dao.contract.PriceListDao;
+import pl.bikepoint.bikeRentalApp.dao.contract.RentalDurationDao;
 import pl.bikepoint.bikeRentalApp.dto.bike.BikeDto;
+import pl.bikepoint.bikeRentalApp.dto.bike.BikeTypeDto;
+import pl.bikepoint.bikeRentalApp.dto.bike.BrandDto;
+import pl.bikepoint.bikeRentalApp.dto.contract.OrderDto;
+import pl.bikepoint.bikeRentalApp.dto.contract.PriceListDto;
+import pl.bikepoint.bikeRentalApp.dto.contract.RentalDurationDto;
 
 public class Mapper {
 
-    private Mapper(){}
+    private Mapper() {
+    }
 
-    public static BikeDao map(BikeDto bikeDto){
+    public static BikeDao map(BikeDto bikeDto) {
         return BikeDao.builder()
                 .id(bikeDto.getId())
                 .name(bikeDto.getName())
@@ -20,7 +31,7 @@ public class Mapper {
                 .build();
     }
 
-    public static BikeDto map(BikeDao bikeDao){
+    public static BikeDto map(BikeDao bikeDao) {
         return BikeDto.builder()
                 .id(bikeDao.getId())
                 .name(bikeDao.getName())
@@ -32,4 +43,94 @@ public class Mapper {
                 .orderDao(bikeDao.getOrderDao())
                 .build();
     }
-}
+
+    public static BikeTypeDto map(BikeTypeDao bikeTypeDao) {
+        return BikeTypeDto.builder()
+                .id(bikeTypeDao.getId())
+                .type(bikeTypeDao.getType())
+                .build();
+    }
+
+    public static BikeTypeDao map(BikeTypeDto bikeTypeDto) {
+        return BikeTypeDao.builder()
+                .id(bikeTypeDto.getId())
+                .type(bikeTypeDto.getType())
+                .build();
+    }
+
+    public static BrandDto map(BrandDao brandDao) {
+        return BrandDto.builder()
+                .id(brandDao.getId())
+                .brandName(brandDao.getBrandName())
+                .bikes(brandDao.getBikes())
+                .build();
+    }
+
+    public static BrandDao map(BrandDto brandDto) {
+        return BrandDao.builder()
+                .id(brandDto.getId())
+                .brandName(brandDto.getBrandName())
+                .bikes(brandDto.getBikes())
+                .build();
+    }
+
+    public static OrderDao map(OrderDto orderDto) {
+        return OrderDao.builder()
+                .id(orderDto.getId())
+                .rentalPrice(orderDto.getRentalPrice())
+                .helmetPrice(orderDto.getHelmetPrice())
+                .pedalsPrice(orderDto.getPedalsPrice())
+                .bikes(orderDto.getBikes())
+                .prices(orderDto.getPrices())
+                .build();
+    }
+
+    public static OrderDto map(OrderDao orderDao) {
+        return OrderDto.builder()
+                .id(orderDao.getId())
+                .rentalPrice(orderDao.getRentalPrice())
+                .helmetPrice(orderDao.getHelmetPrice())
+                .pedalsPrice(orderDao.getPedalsPrice())
+                .bikes(orderDao.getBikes())
+                .prices(orderDao.getPrices())
+                .build();
+    }
+
+    public static PriceListDto map(PriceListDao priceListDao) {
+        return PriceListDto.builder()
+                .id(priceListDao.getId())
+                .rentals(priceListDao.getRentals())
+                .build();
+    }
+
+    public static PriceListDao map(PriceListDto priceListDto) {
+        return PriceListDao.builder()
+                .id(priceListDto.getId())
+                .rentals(priceListDto.getRentals())
+                .build();
+    }
+
+    public static RentalDurationDto map(RentalDurationDao rentalDurationDao) {
+        return RentalDurationDto.builder()
+                .id(rentalDurationDao.getId())
+                .oneDay(rentalDurationDao.getOneDay())
+                .twoDays(rentalDurationDao.getTwoDays())
+                .threeDays(rentalDurationDao.getThreeDays())
+                .priceList(rentalDurationDao.getPriceList())
+                .build();
+    }
+
+    public static RentalDurationDao map(RentalDurationDto rentalDurationDto) {
+        return RentalDurationDao.builder()
+                .id(rentalDurationDto.getId())
+                .oneDay(rentalDurationDto.getOneDay())
+                .twoDays(rentalDurationDto.getTwoDays())
+                .threeDays(rentalDurationDto.getThreeDays())
+                .priceList(rentalDurationDto.getPriceList())
+                .build();
+    }
+
+
+
+
+
